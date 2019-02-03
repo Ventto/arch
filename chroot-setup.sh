4e878ec7a3f392821ab23eda54ca57a0c541f829
Thomas Venriès <thomas.venries@gmail.com>
2019-02-03 16:42:55 +0100

    misc: Add setup scripts

---
+++ chroot-setup.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 Thomas "Ventto" Venriès <thomas.venries@gmail.com>
+#
+set -e
+
+TITLE()
+{
+    printf '\n#===================================#\n'
+    printf '# %s\n' "$1"
+    printf '#===================================#\n\n'
+}
+
+GRUB_INSTALL()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    ln -s /hostrun/lvm /run/lvm
+    ls -l /run/lvm
+    grub-install --efi-directory=/boot/efi
+    grub-mkconfig -o /boot/grub/grub.cfg
+    grub-mkconfig -o /boot/efi/EFI/arch/grub.cfg
+}
+
+SET_LOCALTIME()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+    hwclock --systohc --utc
+}
+
+SET_LOCALE()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+    locale-gen
+    echo "LANG=en_US.UTF-8" > /etc/locale.conf
+}
+
+SET_KEYBOARD_LANG()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    echo "KEYMAP=fr-latin1" > /etc/vconsole.conf
+}
+
+SET_HOSTNAME()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    echo "arch-laptop" > /etc/hostname
+}
+
+ADD_UDEV_RULES()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    mac="$(ip a l | grep -E -A 1 '^[0-9]+: enp.*: <.*' \
+            | sed -n 's#link/ether \([0-9a-f]\{2\}\(:[0-9a-f]\{2\}\)\{5\}\) brd .*#\1#p' \
+            | awk '{print $1}')"
+
+    mkdir -p /etc/udev/rules.d/
+    printf 'SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="%s", NAME="eth0"\n' \
+           "$mac" > /etc/udev/rules.d/10-network.rules
+    printf '# SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="00:11:22:33:44:55", NAME="wlo0"\n' \
+            >> /etc/udev/rules.d/10-network.rules
+}
+
+ADD_USER()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    useradd -m -G wheel -s /bin/zsh ventto
+    echo -n 'ventto:user' | chpasswd
+}
+
+SET_SUDOERS()
+{
+    pacman -S --noconfirm sudo
+    sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
+}
+
+DEL_BEEP()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+}
+
+PACMAN_ENABLE_MULTILIB()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    echo "[multilib]" >> /etc/pacman.conf
+    echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+
+    pacman -Syu --noconfirm
+}
+
+INSTALL_PACMAN_USER_PKGS()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    wget -O pacman-list.pkg https://gist.githubusercontent.com/Ventto/104ccef45f20e424e78ac064d69e6551/raw/pacman-list.pkg
+
+    pacman -S --noconfirm - < pacman-list.pkg
+}
+
+MAIN()
+{
+    {
+        GRUB_INSTALL
+        SET_LOCALTIME
+        SET_LOCALE
+        SET_KEYBOARD_LANG
+        SET_HOSTNAME
+        ADD_UDEV_RULES
+        ADD_USER
+        SET_SUDOERS
+        DEL_BEEP
+        PACMAN_ENABLE_MULTILIB
+        INSTALL_PACMAN_USER_PKGS
+    } 2>&1 | tee chroot-install.log
+}
+
+MAIN
---
+++ setup.sh
@@ -0,0 +1,193 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 Thomas "Ventto" Venriès <thomas.venries@gmail.com>
+#
+set -e
+
+TITLE()
+{
+    printf '\n#===================================#\n'
+    printf '# %s\n' "$1"
+    printf '#===================================#\n\n'
+}
+
+CHECK_CONNECTION()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    ping -c 3 google.com
+}
+
+SET_TIMEDATECTL()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    # Enable network time synchronization
+    timedatectl set-ntp true
+    timedatectl status
+}
+
+ERASING_DISK()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    # Erase the GPT at least and potentially the first bootable partition
+    dd if=/dev/zero of=/dev/sda bs=100M count=10 status=progress
+}
+
+CREATE_PARTITIONS()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    parted /dev/sda --script mklabel gpt
+    parted /dev/sda --script mkpart ESP fat32 1MiB 200MiB
+    parted /dev/sda --script set 1 boot on
+    parted /dev/sda --script name 1 efi
+    parted /dev/sda --script mkpart primary 800MiB 100%
+    parted /dev/sda --script set 2 lvm on
+    parted /dev/sda --script name 2 lvm
+    parted /dev/sda --script print
+}
+
+read_secret()
+{
+    stty -echo
+    trap 'stty echo' EXIT
+    read "$@"
+    stty echo
+    trap - EXIT
+    echo
+}
+
+ENCRYPT_SYSTEM()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    modprobe dm-crypt
+    modprobe dm-mod
+
+    printf "Enter passphrase for system encryption:"
+    read_secret passphrase
+    # shellcheck disable=SC2154
+    echo -n "$passphrase" | cryptsetup -q luksFormat /dev/sda2 -
+    echo -n "$passphrase" | cryptsetup luksOpen /dev/sda2 lvm -
+    pvcreate /dev/mapper/lvm
+    vgcreate vg /dev/mapper/lvm
+    lvcreate -L 1G vg -C y -n swap
+    lvcreate -L 512M vg -n boot
+    lvcreate -L 15G vg -n root
+    lvcreate -l +100%FREE vg -n home
+}
+
+MAKEFS_PARTITIONS()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    mkswap -L swap /dev/mapper/vg-swap
+    mkfs.ext4 /dev/mapper/vg-boot
+    mkfs.ext4 /dev/mapper/vg-root
+    mkfs.ext4 /dev/mapper/vg-home
+    mkfs.fat -F32 /dev/sda1
+}
+
+MOUNT_PARTITIONS()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    swapon /dev/mapper/vg-swap
+    mount /dev/mapper/vg-root /mnt
+    mkdir /mnt/home
+    mount /dev/mapper/vg-home /mnt/home
+    mkdir /mnt/boot
+    mount /dev/mapper/vg-boot /mnt/boot
+    mkdir /mnt/boot/efi
+    mount /dev/sda1 /mnt/boot/efi
+
+    lsblk
+}
+
+INSTALL_ARCHLINUX_BASE_PACKAGES()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    pacstrap /mnt base base-devel wget vim efibootmgr grub --noconfirm
+}
+
+GENERATE_FSTAB()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    genfstab -U -p /mnt > /mnt/etc/fstab
+}
+
+CREATE_INITRD()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    sed -i 's#^HOOKS=(\(.*\))#HOOKS=(\1 encrypt lvm2)#' /mnt/etc/mkinitcpio.conf
+    arch-chroot /mnt mkinitcpio -p linux
+}
+
+GRUB_SETUP_STEP_FIRST()
+{
+    TITLE "Step: ${FUNCNAME[0]}"
+
+    sed -i 's#GRUB_CMDLINE_LINUX="\(.*\)"#GRUB_CMDLINE_LINUX="cryptdevice=/dev/sda2:lvm"#' \
+        /mnt/etc/default/grub
+    echo "GRUB_ENABLE_CRYPTODISK=y" >> /mnt/etc/default/grub
+
+    ##
+    # We need the lvmpad.socket to make `grub-mkconfig` command run properly
+    # under chroot later
+    mkdir /mnt/hostrun
+    mount --bind /run /mnt/hostrun
+}
+
+RUN_CHROOT_SETUP()
+{
+    # Run the second script which runs commands under chroot
+    cp ./chroot-setup.sh /mnt
+    arch-chroot /mnt ./chroot-setup.sh
+}
+
+REMOVE_ROOT_PASSWD()
+{
+    # Disable root's password
+    passwd -R /mnt -l root
+}
+
+PRINT_RESUME()
+{
+    lsblk
+    df -h | grep -E 'Size|vg-|sda'
+}
+
+UMOUNT_PARTITIONS()
+{
+    umount -R /mnt
+    swapoff -a
+}
+
+MAIN()
+{
+    # Display output in terminal and write it to a log file as well
+    {
+        CHECK_CONNECTION
+        SET_TIMEDATECTL
+        ERASING_DISK
+        CREATE_PARTITIONS
+        ENCRYPT_SYSTEM
+        MAKEFS_PARTITIONS
+        MOUNT_PARTITIONS
+        INSTALL_ARCHLINUX_BASE_PACKAGES
+        GENERATE_FSTAB
+        CREATE_INITRD
+        GRUB_SETUP_STEP_FIRST
+        RUN_CHROOT_SETUP
+        REMOVE_ROOT_PASSWD
+        PRINT_RESUME
+        UMOUNT_PARTITIONS
+    } 2>&1 | tee install.log
+}
+
+MAIN
